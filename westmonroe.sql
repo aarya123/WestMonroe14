ef6999b625aa1c37b4957b6653c8cce7fa2c9afc
Anubhaw Arya <anubhaw.arya@gmail.com>
2014-02-22 13:50:03 -0500

    Merge

--- westmonroe.sql
+++ westmonroe.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
@@ -62,62 +61,3 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-=======
--- phpMyAdmin SQL Dump
--- version 4.0.9
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Feb 22, 2014 at 07:43 PM
--- Server version: 5.6.14
--- PHP Version: 5.5.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `westmonroe`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `candidates`
---
-
-CREATE TABLE IF NOT EXISTS `candidates` (
-  `name` varchar(30) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `school` text NOT NULL,
-  `major` text NOT NULL,
-  `gpa` int(11) NOT NULL,
-  `grad_date` int(11) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `events`
---
-
-CREATE TABLE IF NOT EXISTS `events` (
-  `name` text NOT NULL,
-  `description` text NOT NULL,
-  `location` text NOT NULL,
-  `time` date NOT NULL,
-  PRIMARY KEY (`name`(30))
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
->>>>>>> 7950213cf97fe3730e448e71bef78bbbbffc5f07
