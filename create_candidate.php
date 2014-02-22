f29d7f6e0e947f0053f2f0765481afb5f7012aaf
mgottein <mgottein@purdue.edu>
2014-02-22 13:04:23 -0500

    updated create_candidate.php to work

--- create_candidate.php
+++ create_candidate.php
@@ -1,5 +1,10 @@
 <?php
 	include_once("db.php");
+	$required = array('name', 'email', 'school', 'major', 'gpa', 'grad_date');
+	if(count(array_intersect_key(array_flip($required), $_POST)) != count($required)) {
+		echo json_encode(array('error' => 'invalid args'));
+		exit();
+	}
 	try {
 		$db = new PDO(DB_CONN_STR, DB_USER, DB_PASSWORD);
 		$sql_stmt = <<<'EOT'
@@ -16,6 +21,10 @@ EOT;
 				':grad_date' => $_POST['grad_date']
 			)
 		);
+		if($stmt->errorCode() != 0) {
+			echo json_encode(array('error' => $stmt->errorInfo()));
+			exit();
+		}
 		echo json_encode(array('user_id' => $db->lastInsertId()));
 	}
 	catch(PDOException $e) {
