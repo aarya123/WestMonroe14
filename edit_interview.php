6b9c818def1ce796827d73f03b43595933541857
mgottein <mgottein@purdue.edu>
2014-02-22 17:06:45 -0500

    added edit interview endpoint

---
+++ edit_interview.php
@@ -0,0 +1,28 @@
+<?php
+$sql_update_field = "UPDATE Interview SET Interviewer=:interviewer, Notes=:notes, Candidate_id=:candidate_id WHERE id=:interview_id";
+include_once("db.php");
+	$required = array('interview_id', 'candidate_id', 'notes', 'interviewer');
+	if(count(array_intersect_key(array_flip($required), $_POST)) != count($required)) {
+		echo json_encode(array('error' => 'invalid args'));
+		exit();
+	}
+	try {
+		$db = new PDO(DB_CONN_STR, DB_USER, DB_PASSWORD);
+		$stmt = $db->prepare($sql_update_field);
+		$stmt->execute(array(
+				':interview_id' => $_POST['interview_id'],
+				':candidate_id' => $_POST['candidate_id'],
+				':notes' => $_POST['notes'],
+				':interviewer' => $_POST['interviewer']
+			)
+		);
+		if($stmt->errorCode() != 0) {
+			echo json_encode(array('error' => $stmt->errorInfo()));
+			exit();
+		}
+		echo json_encode(array('ok' => ""));
+	}
+	catch(PDOException $e) {
+		echo json_encode(array('error' => $e->getMessage()));
+	}
+?>
