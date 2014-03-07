53d0aca0d892ebebbcb0234f11161c7a21c32aa0
mgottein <mgottein@purdue.edu>
2014-03-07 00:04:46 -0600

    changing metrics

--- getMetrics.php
+++ getMetrics.php
@@ -1,59 +1,63 @@
 <?php include('header.php'); ?>
 <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
 <style>
+/*
 #listContainer {
 	text-align: left;
 	margin-left: 50px;
 }
+*/
+
 </style>
+
+
+
+<!--
 <select id="select"></select>
 <svg id="svg"></svg>
 <div id="listContainer">
 	<h id="listHeading" style="margin: 100px"></h>
 	<ul id="list"></ul>
 </div>
+-->
+
+
 
 <script>
 
-	var dispatch = d3.dispatch("load", "optionchange");
 	var data = <?php include('get_metrics.php'); ?>;
-	var prettyNames = {"school": "School", "grad_date": "Grad Date", "major" : "Major", "gpa" : "GPA", "offer_status" : "Offer Status"};
-	var curLabel = null;
-	var handlers = {"click": function(option, label, data) {
-			if(curLabel != label) {
-				curLabel = label;
-				while(list.firstChild) {
-					list.removeChild(list.firstChild);
-				}
-				data = data.sort(function(a, b) {
-					if(a.name < b.name) {
-						return -1;
-					}
-					else if(a.name > b.name) {
-						return 1;
-					}
-					else {
-						return 0;
-					}
-				});
-				listHeading.innerHTML = prettyNames[option] + " - " + label;
-				for(var i = 0; i < data.length; ++i) {
-					var li = document.createElement('li');
-					var a = document.createElement('a');
-					a.setAttribute('href', '/editCandidate.php#' + data[i].id);
-					a.innerHTML = data[i].name;
-					li.appendChild(a);
-					list.appendChild(li);
-				}
-			}
+	var infos = {"candidate" : {
+		editPage: "/editCandidate.php",
+		selectOptions: {"school": "School", "grad_date": "Grad Date", "major" : "Major", "gpa" : "GPA", "offer_status" : "Offer Status"},
+		dataType: "candidateData"
 		}
 	};
+	function cmpName(a, b) {
+		if(a.name < b.name) {
+			return -1;
+		}
+		else if(a.name > b.name) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
 
-	dispatch.on("optionchange.pie", function(id, data) {
+	function populateList(graph, option, label, data) {
+		if(graph.curLabel != label) {
+			graph.curLabel = label;
+			graph.list.selectAll("li").remove();
+			data = data.sort(cmpName);
+			graph.listHeading.text(graph.info.selectOptions[option] + " - " + label);
+			for(var i = 0; i < data.length; ++i) {
+				graph.list.append("li").append("a").attr("href",graph.info.editPage + "#" + data[i].id).text(data[i].name);
+			}
+		}
+	}
+
+	function countData(id, data) {
 		var dataCount = {};
-		var width = 500;
-		var height = 500;
-		var radius = 250;
 		for(var i = 0; i < data.length; ++i) {
 			var curId = data[i][id];
 			if(!dataCount[curId]) {
@@ -64,59 +68,92 @@
 				dataCount[curId].data.push(data[i]);
 			}
 		}
-		var color = d3.scale.category10()
-			.domain(Object.keys(dataCount));
+		return dataCount;
+	}
 
-		var pie  = d3.layout.pie()
-			.sort(null)
-			.value(function(id) { return dataCount[id].count});
+	function getPieGraphMaker(graph) {
+		return function(id, data) {
+			graph.curLabel = null;
+			var handlers = {"click": function(option, label, data) {
+				populateList(graph, option, label, data);
+			}};
+			var dataCount = countData(id, data);
+			var width = 500;
+			var height = 500;
+			var radius = 250;
+			
+			var color = d3.scale.category10()
+				.domain(Object.keys(dataCount));
 
-		var arc = d3.svg.arc()
-	    .outerRadius(radius - 10)
-	    .innerRadius(0);
+			var pie  = d3.layout.pie()
+				.sort(null)
+				.value(function(id) { return dataCount[id].count});
 
-		var svg = d3.select("#svg")
-	    .attr("width", width)
-	    .attr("height", height)
-	  	.append("g")
-	    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+			var arc = d3.svg.arc()
+		    .outerRadius(radius - 10)
+		    .innerRadius(0);
 
-	    var g = svg.selectAll(".arc")
-	    .data(pie(Object.keys(dataCount)))
-	    .enter()
-	    .append("g").attr("class", "arc");
+			var svg = graph.svg
+		    .attr("width", width)
+		    .attr("height", height)
+		  	.append("g")
+		    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+		    var g = svg.selectAll(".arc")
+		    .data(pie(Object.keys(dataCount)))
+		    .enter()
+		    .append("g").attr("class", "arc");
 
 
-	    var path = g.append("path")
-	      .attr("d", arc)
-	      .style("fill", function(d) { return color(d.data); });
-	    for(var e in handlers) {
-	    	path.on(e, function(d) {
-	    		handlers[e](id, d.data, dataCount[d.data].data);
-	    	});
-	    }
+		    var path = g.append("path")
+		      .attr("d", arc)
+		      .style("fill", function(d) { return color(d.data); });
+		    for(var e in handlers) {
+		    	path.on(e, function(d) {
+		    		handlers[e](id, d.data, dataCount[d.data].data);
+		    	});
+		    }
 
-	  	g.append("text")
-	      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-	      .attr("dy", ".35em")
-	      .style("text-anchor", "middle")
-	      .text(function(d) { return d.data; });
-	});
-	
+		  	g.append("text")
+		      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+		      .attr("dy", ".35em")
+		      .style("text-anchor", "middle")
+		      .text(function(d) { return d.data; });
+	  	}
+	}
 
-	dispatch.on("load.menu", function(data) {
-		var select = d3.select("#select")
-		.on("change", function() { dispatch.optionchange(this.value, data.data) });
+	function getSelector(graph) {
+		return function(data) {
+			var select = graph.select.on("change", function() { graph.dispatch.optionchange(this.value, data[graph.info.dataType]) });
 
-		select.selectAll("option")
-			.data(data.select)
-			.enter().append("option")
-			.attr("value", function(d) { return d; })
-			.text(function(d) { return prettyNames[d]; });
-	});
+			select.selectAll("option")
+				.data(Object.keys(graph.info.selectOptions))
+				.enter().append("option")
+				.attr("value", function(d) { return d; })
+				.text(function(d) { return graph.info.selectOptions[d]; });
+		}
+	}
 
-	dispatch.load(data);
-	dispatch.optionchange(data.select[0], data.data);
+
+	function initPieGraph(graph) {
+		graph.dispatch = d3.dispatch("load", "optionchange");
+		graph.dispatch.on("optionchange.pie", getPieGraphMaker(graph));
+		graph.dispatch.on("load.menu", getSelector(graph));
+		graph.dispatch.load(data);
+		graph.dispatch.optionchange(Object.keys(graph.info.selectOptions)[0], data[graph.info.dataType]);
+	}
+
+	function createGraph(info) {
+		var graph = d3.select("main").append("div");
+		var select = graph.append("select");
+		var svg = graph.append("svg");
+		var listContainer = graph.append("div");
+		var listHeading = graph.append("h");
+		var list = graph.append("ul");
+		return {"info" : info, "select" : select, "svg" : svg, "listContainer" : listContainer, "listHeading" : listHeading, "list" : list};
+	}
+
+	initPieGraph(createGraph(infos.candidate));
 
 </script>
 <?php include('footer.php'); ?>
--- get_metrics.php
+++ get_metrics.php
@@ -2,13 +2,28 @@
 include_once("db.php");
 try {
 	$db = new PDO(DB_CONN_STR, DB_USER, DB_PASSWORD);
-	$stmt = $db->prepare('SELECT id, name, school, major, gpa, DATE_FORMAT(grad_date, "%b %y") AS grad_date, offer_status FROM candidate');
-	$stmt->execute();
-	if($stmt->errorCode() != 0) {
-		echo json_encode(array('error' => $stmt->errorInfo()));
+	$candidateData = $db->prepare('SELECT id, name, school, major, gpa, DATE_FORMAT(grad_date, "%b %y") AS grad_date, offer_status FROM candidate');
+	$eventData = $db->prepare('SELECT id, name, location, DATE_FORMAT(time, "%y %b %d") AS time, description FROM event');
+	$eventAttendanceData = $db->prepare('SELECT Event_id, Candidate_id, notes, attended FROM  event_has_candidate');
+	$candidateData->execute();
+	if($candidateData->errorCode() != 0) {
+		echo json_encode(array('error' => $candidateData->errorInfo()));
 		exit();
 	}
-	echo json_encode(array("select" => array("school", "major", "gpa", "grad_date", "offer_status"), "data" => $stmt->fetchAll(PDO::FETCH_ASSOC)));
+	$eventData->execute();
+	if($eventData->errorCode() != 0) {
+		echo json_encode(array('error' => $eventData->errorInfo()));
+		exit();
+	}
+	$eventAttendanceData->execute();
+	if($eventAttendanceData->errorCode() != 0) {
+		echo json_encode(array('error' => $eventAttendanceData->errorInfo()));
+	}
+	echo json_encode(array(
+	 "candidateData" => $candidateData->fetchAll(PDO::FETCH_ASSOC),
+	 "eventData" => $eventData->fetchAll(PDO::FETCH_ASSOC),
+	 "eventAttendanceData" => $eventAttendanceData->fetchAll(PDO::FETCH_ASSOC)
+	 ));
 }
 catch(PDOException $e) {
 	echo json_encode(array('error' => $e->getMessage()));
