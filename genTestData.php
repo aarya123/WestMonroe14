f1b156aef29c271843c6a025b9f756e2ac96c5e4
mgottein <mgottein@purdue.edu>
2014-03-07 02:59:01 -0600

    added number and date data types to metrics

--- genTestData.php
+++ genTestData.php
@@ -2,11 +2,18 @@
 include_once("db.php");
 	try {
 		$db = new PDO(DB_CONN_STR, DB_USER, DB_PASSWORD);
+		$db->beginTransaction();
 		$stmt = $db->prepare("INSERT INTO Candidate (name, email, school, major, gpa, grad_date, offer_status) VALUES (:name, :email, :school, :major, :gpa, :grad_date, :offer_status)");
+		$eventStmt = $db->prepare("INSERT INTO Event (name, location, time, description) VALUES (:name, :location, :time, :description)");
+		$eventCandidateStmt = $db->prepare("INSERT INTO Event_has_Candidate (Event_id, Candidate_id, notes, attended) VALUES (:event_id, :candidate_id, :notes, :attended)");
 		$schools = array('purdue', 'iu', 'illinois', 'depaw', 'northwestern', 'iupui');
 		$majors = array('cs', 'business', 'english', 'math', 'science', 'history', 'economics');
 		$gradDates = array('2015-05-01', '2015-12-01', '2016-05-01', '2016-12-01', '2017-05-01', '2017-12-01');
+		$eventDates = array('2015-02-01', '2015-03-01', '2015-04-01', '2015-01-01');
+		$eventLocations = array('loc a', 'loc b', 'loc c');
 		$offer_statuses = array('None', 'Pending', 'No Offer', 'Accepted', 'Rejected');
+		$candidateId = array();
+		$eventId = array();
 		for($i = 0; $i < 200; ++$i) {
 			$name = (string)rand(0, 200);
 			$email = ((string)rand(0, 200)) . '@' . ((string)rand(0, 200)) . '.com';
@@ -25,13 +32,43 @@ include_once("db.php");
 				':offer_status' => $offer_status
 			));
 			if($stmt->errorCode() != 0) {
-				echo json_encode(array('error' => $stmt->errorInfo()));
-				exit();
+				throw new Exception($stmt->errorInfo());
 			}
+			array_push($candidateId, $db->lastInsertId());
 		}
+		for($i = 0; $i < 50; ++$i) {
+			$name = (string)rand(0, 200);
+			$location = $eventLocations[rand(0, count($eventLocations) - 1)];
+			$time = $eventDates[rand(0, count($eventDates) - 1)];
+			$desc = (string)rand(0, 200);
+			$eventStmt->execute(array(
+				':name' => $name,
+				':location' => $location,
+				':time' => $time,
+				':description' => $desc
+			));
+			if($stmt->errorCode() != 0) {
+				throw new Exception($stmt->errorInfo());
+			}
+			array_push($eventId, $db->lastInsertId());
+		}
+		for($i = 0; $i < 100; ++$i) {
+			$notes = (string)rand(0, 200);
+			$curCandidateId = $candidateId[rand(0, count($candidateId) - 1)];
+			$curEventId = $eventId[rand(0, count($eventId) - 1)];
+			$attended = rand(0, 1);
+			$eventCandidateStmt->execute(array(
+				':notes' => $notes,
+				':candidate_id' => $curCandidateId,
+				':event_id' => $curEventId,
+				':attended' => $attended
+			));
+		}
+		$db->commit();
 		echo "ok";
 	}
-	catch(PDOException $e) {
+	catch(Exception $e) {
+		$db->rollback();
 		echo json_encode(array('error' => $e->getMessage()));
 	}
 ?>
--- getMetrics.php
+++ getMetrics.php
@@ -27,11 +27,20 @@
 
 	var allData = <?php include('get_metrics.php'); ?>;
 	var dataTypes = {"candidate" : {
-		editPage: "/editCandidate.php",
-		selectOptions: {"school": "School", "grad_date": "Grad Date", "major" : "Major", "gpa" : "GPA", "offer_status" : "Offer Status"},
-		dataType: "candidateData"
+			editPage: "/editCandidate.php",
+			selectOptions: {"school": "School", "grad_date": "Grad Date", "major" : "Major", "gpa" : "GPA", "offer_status" : "Offer Status"},
+			optionTypes: {"school" : "category", "grad_date": "date", "major" : "category", "gpa" : "number", "offer_status" : "category"},
+			name: "candidateData"
 		}
 	};
+
+	var months = {"Jan" : 1, "Feb" : 2, "Mar" : 3, "Apr" : 4, "May" : 5, "Jun" : 6, "Jul" : 7, "Aug" : 8, "Sep" : 9, "Oct" : 10, "Nov" : 11, "Dec" : 12};
+
+	function dateToValid(str) {
+		var split = str.split(" ");
+		return split[0] + " 1, " + split[1];
+	}
+
 	var graphList = [];
 
 	function cmpName(a, b) {
@@ -58,28 +67,27 @@
 		}
 	}
 
-	function filterData(id, data) {
-		var filteredData = {};
+	function countData(id, data) {
+		var countedData = {};
 		for(var i = 0; i < data.length; ++i) {
 			var curId = data[i][id];
-			if(!filteredData[curId]) {
-				filteredData[curId] = {count: 1, data: [data[i]]};
+			if(!countedData[curId]) {
+				countedData[curId] = {count: 1, data: [data[i]]};
 			}
 			else {
-				filteredData[curId].count++;
-				filteredData[curId].data.push(data[i]);
+				countedData[curId].count++;
+				countedData[curId].data.push(data[i]);
 			}
 		}
-		return filteredData;
+		return countedData;
 	}
 
-	var handlers = {"click": function(graph, option, label, data) {
-		populateList(graph, option, label, data);
+	function addNextGraph(graph, option, data) {
 		var newData = {};
 		for(var k in graph.data) {
 			newData[k] = graph.data[k];
 		}
-		newData[graph.dataType.dataType] = data;
+		newData[graph.dataType.name] = data;
 		var newDataType = {};
 		for(var k in graph.dataType) {
 			newDataType[k] = graph.dataType[k];
@@ -93,11 +101,76 @@
 		if(Object.keys(newDataType.selectOptions).length > 0) {
 			addGraph(graph.i + 1, newData, graph.chartType, newDataType);
 		}
+	}
+
+	var handlers = {"click": function(graph, option, label, data) {
+		if(graph.dataType.optionTypes[option] != "category") {
+			return;
+		}
+		populateList(graph, option, label, data);
+		addNextGraph(graph, option, data);
 	}};
 
 	function getSelector(graph) {
 		return function(data) {
-			var select = graph.select.on("change", function() { graph.dispatch.optionchange(this.value, data[graph.dataType.dataType]) });
+			data = data[graph.dataType.name];
+			var select = graph.select.on("change", function() {
+				if(graph.selectContainer) {
+					graph.selectContainer.remove();
+					delete graph.selectContainer;
+				}
+				var option = this.value;
+				if(graph.dataType.optionTypes[option] == "number") {
+					var selectContainer = graph.container.insert("div", "svg");
+					var lessValue = selectContainer.append("input").attr("placeholder", "Least Value").attr("type", "number");
+					var greatValue = selectContainer.append("input").attr("placeholder", "Greatest Value").attr("type", "number");
+					var filterButton = selectContainer.append("button").text("Filter");
+					filterButton.on("click", function() {
+						var less = parseInt(lessValue[0][0].value), great = parseInt(greatValue[0][0].value);
+						if(isNaN(less) || less < 0 || great <= 0 || isNaN(great) || great < less) {
+							return;
+						}
+						var newData = [];
+						for(var i = 0; i < data.length; ++i) {
+							if(data[i][option] >= less && data[i][option] <= great) {
+								newData.push(data[i]);
+							}
+						}
+						if(newData.length > 0) {
+							populateList(graph, option, less + " to " + great, newData);
+							addNextGraph(graph, option, newData);
+						}
+					});
+					graph.selectContainer = selectContainer;
+				}
+				else if(graph.dataType.optionTypes[option] == "date") {
+					var selectContainer = graph.container.insert("div", "svg");
+					var lessValue = selectContainer.append("input").attr("placeholder", "Earliest Date").attr("type", "date");
+					var greatValue = selectContainer.append("input").attr("placeholder", "Latest Date").attr("type", "date");
+					var filterButton = selectContainer.append("button").text("Filter");
+					filterButton.on("click", function() {
+						var less = Date.parse(lessValue[0][0].value), great = Date.parse(greatValue[0][0].value);
+						console.log(less, great);
+						if(isNaN(less) || isNaN(great) || great < less) {
+							return;
+						}
+						var newData = [];
+						for(var i = 0; i < data.length; ++i) {
+							if(Date.parse(dateToValid(data[i][option])) >= less && Date.parse(dateToValid(data[i][option])) <= great) {
+								newData.push(data[i]);
+							}
+						}
+						if(newData.length > 0) {
+							populateList(graph, option, lessValue[0][0].value + " to " + greatValue[0][0].value, newData);
+							addNextGraph(graph, option, newData);
+						}
+					});
+					graph.selectContainer = selectContainer;
+				}
+				else {
+					graph.dispatch.optionchange(option, data);
+				}
+			});
 
 			select.selectAll("option")
 				.data(Object.keys(graph.dataType.selectOptions))
@@ -109,17 +182,17 @@
 
 	var chartTypes = {"pie" : function(graph) {
 		return function(id, data) {
-			var filteredData = filterData(id, data);
+			var countedData = countData(id, data);
 			var width = 500;
 			var height = 500;
 			var radius = 250;
 			
 			var color = d3.scale.category10()
-				.domain(Object.keys(filteredData));
+				.domain(Object.keys(countedData));
 
 			var pie  = d3.layout.pie()
 				.sort(null)
-				.value(function(id) { return filteredData[id].count});
+				.value(function(id) { return countedData[id].count});
 
 			var arc = d3.svg.arc()
 		    .outerRadius(radius - 10)
@@ -132,7 +205,7 @@
 		    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 		    var g = svg.selectAll(".arc")
-		    .data(pie(Object.keys(filteredData)))
+		    .data(pie(Object.keys(countedData)))
 		    .enter()
 		    .append("g").attr("class", "arc");
 
@@ -142,7 +215,7 @@
 		      .style("fill", function(d) { return color(d.data); });
 		    for(var e in handlers) {
 		    	path.on(e, function(d) {
-		    		handlers[e](graph, id, d.data, filteredData[d.data].data);
+		    		handlers[e](graph, id, d.data, countedData[d.data].data);
 		    	});
 		    }
 
@@ -167,7 +240,7 @@
 			}
 		}
 		var graphContainer = d3.select("main").append("div");
-		graphContainer.style("width", "700px");
+		graphContainer.style("width", "1000px");
 		var select = graphContainer.append("select");
 		var svg = graphContainer.append("svg");
 		var listContainer = graphContainer.append("div");
@@ -176,7 +249,7 @@
 		listHeading.style("text-align", "left");
 		var list = listContainer.append("ul");
 		list.style("max-height", "300px");
-		list.style("width", "150px");
+		list.style("width", "200px");
 		list.style("overflow", "auto");
 		var graph = {"i" : i, "container": graphContainer, "data" : data, "chartType" : chartType, "dataType" : dataType, "select" : select, "svg" : svg, "listContainer" : listContainer, "listHeading" : listHeading, "list" : list};
 		graphList[i] = graph;
@@ -184,7 +257,7 @@
 		graph.dispatch.on("optionchange", chartTypes[graph.chartType](graph));
 		graph.dispatch.on("load.menu", getSelector(graph));
 		graph.dispatch.load(data);
-		graph.dispatch.optionchange(Object.keys(graph.dataType.selectOptions)[0], data[graph.dataType.dataType]);
+		graph.dispatch.optionchange(Object.keys(graph.dataType.selectOptions)[0], data[graph.dataType.name]);
 	}
 
 	addGraph(0, allData, "pie", dataTypes.candidate);
