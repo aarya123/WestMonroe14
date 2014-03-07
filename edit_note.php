6b36427ac774b35ac109b9b5b3d2504133075cd7
Anubhaw Arya <anubhaw.arya@gmail.com>
2014-03-07 00:27:24 -0600

    Fixed redirection

--- edit_note.php
+++ edit_note.php
@@ -19,10 +19,10 @@ include_once("db.php");
 			echo json_encode(array('error' => $stmt->errorInfo()));
 			exit();
 		}
-		echo json_encode(array('ok' => ""));
+		header("Location: editNote.php");
+ 		die();
 	}
 	catch(PDOException $e) {
 		echo json_encode(array('error' => $e->getMessage()));
 	}
-header('Location: /');
 ?>
